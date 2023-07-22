170e1c90f63ed5d5f387205ffb9f62a4bf820300
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-07-22 18:12:02 +0900

    feat: 交換日記のGETとPOSTのAPIを簡易的に作った。

--- app/api/dairies/route.ts
+++ app/api/dairies/route.ts
@@ -1,12 +1,29 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/app/lib/prisma';
 
-export async function GET(request: Request) {
-    let reqJson = request.json();
+export async function GET(request: NextRequest) {
+    let param = request.nextUrl.searchParams;
     //ここでPrisma.findUniqueかな？
-    return NextResponse.json(reqJson);
+    //TODO: パラメーターの仕様決まったら！
+    let foundData = await prisma.usdDairies.findUnique( {
+        where: {
+            id: 11,
+            user_id: "",
+            posted_at: new Date()
+        }
+    });
+    return NextResponse.json(foundData);
 }
-export async function POST(request: Request) {
-    let reqJson = request.json();
+export async function POST(request: NextRequest) {
+    let reqJson = request.nextUrl;
     //ここでPrisma.createかな？
-    return NextResponse.json(reqJson);
+    //TODO: パラメーターの仕様決まったら！
+    let createdData = await prisma.usdDairies.create({
+        data: {
+            user_id: "id",
+            posted_at: new Date(),
+            main_content: "main content",
+        }
+    });
+    return NextResponse.json(createdData);
 }
