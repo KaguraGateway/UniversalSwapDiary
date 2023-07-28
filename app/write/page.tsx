2bdaeae33cd5b4ce12eef11dfbe26b3ffe05df6b
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-07-29 04:39:02 +0900

    feat: 交換送信機能実装

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,6 +1,10 @@
 import Foundation from "../components/diary/Foundation";
 import Header from "../components/layout/Header";
 
+export const metadata = {
+    title: "日記を書く"
+}
+
 const diaryPage = () => {
     return (
         <>
