ce31b1a8851b5c9f2aab308e40b5b032be74b0a2
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-07-29 06:43:14 +0900

    ページごとにロゴ表示するか否か実装

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,9 +1,18 @@
-import '../auth/auth.css'
+'use client';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+    const pathname = usePathname();
+    const isLogoDisplay = !pathname?.startsWith('/auth');
+
     return (
         <header>
-            <div className="bg-[url('/lace.svg')] bg-repeat h-[76px]" />
+            <div className="bg-[url('/lace.svg')] bg-repeat h-[76px] flex justify-center" style={{ height: isLogoDisplay ? "92px" : "" }}>
+                {
+                    isLogoDisplay && <Image src="/logo.svg" alt="logo" width={160} height={47} className="h-[47px] mt-1" />
+                }
+            </div>
         </header>
     );
 };
