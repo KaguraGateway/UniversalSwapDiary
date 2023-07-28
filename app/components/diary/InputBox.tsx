5cc470ce70c1ed3e17f561eb60c75b8f713d3607
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-07-28 19:04:20 +0900

    feat: KawaiiBox

---
+++ app/components/diary/InputBox.tsx
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import KawaiiBox from "./KawaiiBox";
+
+type Props = {
+    color: string;
+    setInput: Dispatch<SetStateAction<string>>;
+}
+
+const InputBox = (props: Props) => {
+    function valueChange(value: string) {
+        props.setInput(value);
+    }
+    return(
+        <>
+            <KawaiiBox color={props.color} isBorder={true}>
+                <input                 
+                    type="text"
+                    className="text-black w-[100%] h-[100%]"
+                    onChange={(e) => valueChange(e.target.value)}>
+                </input>
+            </KawaiiBox>
+        </>
+    );
+};
+
+export default InputBox;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link"
+"use client"
+
 import LatestDiary from "../components/diary/LatestDiary"
 import Header from "../components/layout/Header"
 import Footer from "../components/layout/Footer"
+import InputBox from "../components/diary/InputBox"
+import { useState } from "react"
 
 const home = () => {
+    const [a, setA] = useState("");
     return (
         <>
             <Header />
+            <InputBox color="" setInput={setA}/>
             <div className="flex flex-col items-center justify-center h-full">
                 <LatestDiary />
             </div>
