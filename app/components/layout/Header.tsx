76a5692b38d41df8304b5d63b8f13d532786e7cb
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-07-29 07:04:35 +0900

    HeaderのBG修正

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import styles from './Header.module.css';
 
 const Header = () => {
     const pathname = usePathname();
@@ -8,7 +9,7 @@ const Header = () => {
 
     return (
         <header>
-            <div className="bg-[url('/lace.svg')] bg-repeat h-[76px] flex justify-center" style={{ height: isLogoDisplay ? "92px" : "" }}>
+            <div className={`bg-[url('/lace.svg')] bg-no-repeat h-[76px] flex justify-center ${styles.header}`} style={{ height: isLogoDisplay ? "92px" : "" }}>
                 {
                     isLogoDisplay && <Image src="/logo.svg" alt="logo" width={160} height={47} className="h-[47px] mt-1" />
                 }
