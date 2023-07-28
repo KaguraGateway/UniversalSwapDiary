f8891ef289ae16e7d17837a0c2985e96b86990a8
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-07-29 04:18:35 +0900

    fix: フォントが適用されないのを修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Zen_Maru_Gothic } from 'next/font/google'
-
-const zenMaru = Zen_Maru_Gothic({ weight: ["300", "400", "500", "700", "900"], display: "swap", preload: false })
 
 export const metadata: Metadata = {
-    title: '無差別交換日記',
+    title: {
+        default: '無差別交換日記',
+        template: '%s | 無差別交換日記',
+    },
     description: 'むさべつに日記がおくられてきます',
 }
 
@@ -15,8 +15,12 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body className={zenMaru.className}>{children}</body>
+        <html lang="ja">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+            </head>
+            <body className='font-zen'>{children}</body>
         </html>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'Zen': ['Zen Maru Gothic', 'sans-serif'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'background' : 'url(../public/background2.png)',
-        'kumo' : 'url(../public/kumo.png)',
-        'lace' : 'url(../public/lace.svg)',
-        'lace_invert' : 'url(../public/lace_invert.svg)',
-      },
-      border: {
-        'loborder': '3px solid rgba(255, 250, 214, 0.60);'
-      }
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'zen': "Zen Maru Gothic",
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+                'background': 'url(../public/background2.png)',
+                'kumo': 'url(../public/kumo.png)',
+                'lace': 'url(../public/lace.svg)',
+                'lace_invert': 'url(../public/lace_invert.svg)',
+            },
+            border: {
+                'loborder': '3px solid rgba(255, 250, 214, 0.60);'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
