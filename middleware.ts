2cb2c6abd13d2719274c0fa274a90a39e3ba7591
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-07-29 02:32:48 +0900

    feat: 未ログイン時のリダイレクト処理を実装

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname.endsWith(".svg") || request.nextUrl.pathname.endsWith(".png")) {
+        return NextResponse.next();
+    }
+
+    const response = NextResponse.next();
+    const supabase = createMiddlewareClient({ req: request, res: response });
+    const { data, error } = await supabase.auth.getSession();
+
+    if (error || data.session == null) {
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
+    // ログイン済みなら/homeにリダイレクト
+    if (request.nextUrl.pathname === '/') {
+        return NextResponse.redirect(new URL('/home', request.url));
+    }
+
+    return response;
+}
+
+export const config = {
+    matcher: ['/((?!api|auth|_next/static|_next/image|favicon.ico|public).*)']
+}
