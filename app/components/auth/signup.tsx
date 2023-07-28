b8f2afc13c3c65233f1a9a79387c25450c48fed1
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-07-28 17:00:34 +0900

    サインアップ修正

--- app/components/auth/signup.tsx
+++ app/components/auth/signup.tsx
@@ -2,15 +2,14 @@
 import Link from 'next/link';
 import './auth.css'
 import React, { useState, FormEvent } from 'react';
-
 const Signup = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // ここでAPIを呼び出したり、状態を更新したりします。
-        console.log(`Email: ${email}, Password: ${password}`);
+        console.log(`Id: ${name}, Email: ${email}, Password: ${password}`);
     };
     return (
         <div className="h-screen w-full bg-custom font-Zen">
@@ -22,12 +21,25 @@ const Signup = () => {
                         <img src="/logo.png" alt="logo"/>
                     </div>
                     <h2 className=' py-4 text-center text-2xl font-bold'>とうろく</h2>
-                    <form onSubmit={handleSubmit} className="bg-[url('/kumo1.png')] h-72 bg-no-repeat bg-center bg-cover flex flex-col items-center justify-center">
+                    <form onSubmit={handleSubmit} className="bg-[url('/kumo1.png')] h-80 bg-no-repeat bg-center bg-cover flex flex-col items-center justify-center">
                         <div className='w-full px-12'>
                             <div>
-                                <div className='mb-4'>
-                                    <label htmlFor="email" className='block mb-2 text-xl text-shadow'>
+                                <div className='mb-2'>
+                                    <label htmlFor="email" className='block mb-1 text-xl text-shadow'>
                                         なまえ
+                                    </label>
+                                    <input
+                                        id="name"
+                                        type="name"
+                                        className='text-black bg-transparent w-full form-border text-shadow'
+                                        value={name}
+                                        onChange={e => setName(e.target.value)}
+                                        placeholder="ニックネーム"
+                                    />
+                                </div>
+                                <div className='mb-2 pt-1'>
+                                    <label htmlFor="password" className='block mb-1 text-xl text-shadow'>
+                                        メールアドレス
                                     </label>
                                     <input
                                         id="email"
@@ -38,8 +50,8 @@ const Signup = () => {
                                         placeholder="Email"
                                     />
                                 </div>
-                                <div className='mb-4 pt-2'>
-                                    <label htmlFor="password" className='block mb-2 text-xl pt-2 text-shadow'>
+                                <div className='pt-1'>
+                                    <label htmlFor="password" className='block mb-1 text-xl pt-2 text-shadow'>
                                         あいことば
                                     </label>
                                     <input
@@ -48,25 +60,27 @@ const Signup = () => {
                                         className='text-black bg-transparent w-full form-border text-shadow'
                                         value={password}
                                         onChange={e => setPassword(e.target.value)}
-                                        placeholder="Password" 
+                                        placeholder="Password"
                                     />
                                 </div>
                             </div>
-                            <button type="submit" className='auth-button text-2xl font-normal'>
-                                <Link href="/auth/login">
-                                    とうろく
-                                </Link>
-                            </button>
+                            <div className=' mt-2'>
+                                <button type="submit" className='auth-button text-2xl font-normal'>
+                                    <Link href="/auth/login">
+                                        とうろく
+                                    </Link>
+                                </button>
+                            </div>
                         </div>
                     </form>
-                    <div className='flex flex-col items-center py-10'>
+                    <div className='flex flex-col items-center py-6'>
                         <button type="submit" className='google-button items-center mb-4 inline-flex'>
-                            <img src="/Google.svg" alt="Google" className="mr-2 w-5 h-5"/> 
+                            <img src="/Google.svg" alt="Google" className="mr-2 w-5 h-5"/>
                             <Link href="/auth/login">
                                 ぐーぐるでとうろく
                             </Link>
                         </button>
-                        <button className='pt-4'>
+                        <button>
                             <Link href="/auth/login">
                                 ろぐいんはこちら
                             </Link>
@@ -82,5 +96,4 @@ const Signup = () => {
         </div>
     );
 };
-
 export default Signup;
