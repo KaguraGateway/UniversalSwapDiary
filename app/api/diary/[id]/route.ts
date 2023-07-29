a594090b86799d1f1969a20b2160d3bad42b044c
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-07-29 14:36:15 +0900

    feat: diaryページを実装した

--- app/api/diary/[id]/route.ts
+++ app/api/diary/[id]/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { supabaseAdmin } from "@/lib/supabase";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
@@ -6,15 +7,17 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest, { params }: { params: { id: number } }) {
     const supabase = createRouteHandlerClient({ cookies });
     const { data, error } = await supabase.auth.getSession();
-    const userId = data.session?.user.id;
+    const userId = data.session?.user.id; console.log(params);
 
     if (error || userId == null) {
         return new NextResponse(null, { status: 401 });
+    } else if (params.id == null) {
+        return new NextResponse(null, { status: 400 });
     }
 
     const foundData = await prisma.usdDiaries.findUnique({
         where: {
-            id: params.id
+            id: Number(params.id)
         },
         include: {
             receive_user: true
@@ -27,5 +30,7 @@ export async function GET(request: NextRequest, { params }: { params: { id: numb
     // 送信前に消す
     foundData.receive_user = null;
 
-    return NextResponse.json(foundData);
+    const user = await supabaseAdmin.auth.admin.getUserById(foundData.user_id);
+
+    return NextResponse.json({ ...foundData, user_name: user.data.user?.user_metadata.username });
 }
