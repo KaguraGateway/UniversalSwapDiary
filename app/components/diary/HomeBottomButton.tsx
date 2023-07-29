0bbd88caa1434d0383c93a89dc531ebe5501f343
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-07-29 14:03:57 +0900

    feat: HomeButtonBottom

---
+++ app/components/diary/HomeBottomButton.tsx
@@ -0,0 +1,18 @@
+import { KawaiiButton } from "@/ui/KawaiiButton";
+import Link from "next/link";
+import { KawaiiTitle } from "@/ui/KawaiiTitle";
+
+const HomeBottomButton = () => {
+    return(
+        <>
+            <Link href={"/memory"} className="flex flex-col gap-y-8 items-center">
+                <KawaiiButton border={true} className="border-[#6A5138] bg-[#E8C6A2] w-[291px]"><KawaiiTitle size={"lg"}>おもいでをみる</KawaiiTitle></KawaiiButton>
+            </Link>
+            <Link href={"/home"}>
+                <KawaiiButton border={true} className="border-[#F5F5F5] bg-[#FFB8C1] w-[252px]">ホームにもどる</KawaiiButton>
+            </Link>
+        </>
+    );
+};
+
+export default HomeBottomButton;
