4e7cbb0aad5f1eebf0491e4994cddc22ae902a2a
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-07-29 12:05:08 +0900

    feat: Success Boder fix

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -6,18 +6,16 @@ import { KawaiiButton } from '@/ui/KawaiiButton'
 const success = () => {
     return (
         <>
-            <KawaiiPage>
-                <div className="flex items-center flex-col mt-[50px]">
-                    <Image src="/SuccessMark.png" width={133} height={102} alt='成功マーク'/>
-                    <div className="mt-[42px]">
-                        <KawaiiTitle color={'#7A5648'} size={'3xl'}>こうかんしました</KawaiiTitle>
-                    </div>
-                    <div className="mt-[80px] flex items-center flex-col gap-y-5">
-                        <KawaiiButton color={'#E9C6A3'} border={true} className='border-[#E9C6A3] bg-[#FFE1C2] border-4'><KawaiiTitle size={'2xl'}>にっきをみる</KawaiiTitle></KawaiiButton>
-                        <KawaiiButton color={'#7A5648'} border={true} className='border-4'>ホーム</KawaiiButton>
-                    </div>
+            <div className="flex items-center flex-col mt-[50px]">
+                <Image src="/SuccessMark.png" width={133} height={102} alt='成功マーク'/>
+                <div className="mt-[42px]">
+                    <KawaiiTitle color={'#7A5648'} size={'3xl'}>こうかんしました</KawaiiTitle>
                 </div>
-            </KawaiiPage>
+                <div className="mt-[80px] flex items-center flex-col gap-y-5">
+                    <KawaiiButton color={'#E9C6A3'} border={true} className='border-[#E9C6A3] bg-[#FFE1C2] border-4'><KawaiiTitle size={'2xl'}>にっきをみる</KawaiiTitle></KawaiiButton>
+                    <KawaiiButton color={'#7A5648'} border={true} className='border-4'>ホーム</KawaiiButton>
+                </div>
+            </div>
         </>
     )
 }
