2bdd04f566d28ebafa7e83386799db7343d7471e
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-07-29 13:34:24 +0900

    feat: BottomButton in home bug

--- app/components/diary/HomeBottomButton.tsx
+++ app/components/diary/HomeBottomButton.tsx
@@ -4,10 +4,14 @@ import { KawaiiTitle } from "@/ui/KawaiiTitle";
 
 const HomeBottomButton = () => {
     return(
-        <Link href={"/memory"} className="flex flex-col gap-y-8 items-center">
-            <KawaiiButton border={true} className="border-[#6A5138] bg-[#E8C6A2] w-[291px]"><KawaiiTitle size={"lg"}>おもいでをみる</KawaiiTitle></KawaiiButton>
-            <KawaiiButton border={true} className="border-[#F5F5F5] bg-[#FFB8C1] w-[252px]">ホームにもどる</KawaiiButton>
-        </Link>
+        <>
+            <Link href={"/memory"} className="flex flex-col gap-y-8 items-center">
+                <KawaiiButton border={true} className="border-[#6A5138] bg-[#E8C6A2] w-[291px]"><KawaiiTitle size={"lg"}>おもいでをみる</KawaiiTitle></KawaiiButton>
+            </Link>
+            <Link href={"/home"}>
+                <KawaiiButton border={true} className="border-[#F5F5F5] bg-[#FFB8C1] w-[252px]">ホームにもどる</KawaiiButton>
+            </Link>
+        </>
     );
 };
 
