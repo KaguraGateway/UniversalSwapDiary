e6595314e42ec1ba6b6a29c57d9ade9160993999
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-07-29 12:03:47 +0900

    feat: Success

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,17 +1,23 @@
-import Link from 'next/link'
-import Header from '../components/layout/Header'
+import KawaiiPage from '../components/layout/KawaiiPage'
+import Image from 'next/image'
+import { KawaiiTitle } from '@/ui/KawaiiTitle'
+import { KawaiiButton } from '@/ui/KawaiiButton'
 
 const success = () => {
     return (
         <>
-            <Header />
-            <h2>Scuessページ</h2>
-            <div className="success">
-                <h1>Success</h1>
-                <Link href="/home">
-                    homeへ
-                </Link>
-            </div>
+            <KawaiiPage>
+                <div className="flex items-center flex-col mt-[50px]">
+                    <Image src="/SuccessMark.png" width={133} height={102} alt='成功マーク'/>
+                    <div className="mt-[42px]">
+                        <KawaiiTitle color={'#7A5648'} size={'3xl'}>こうかんしました</KawaiiTitle>
+                    </div>
+                    <div className="mt-[80px] flex items-center flex-col gap-y-5">
+                        <KawaiiButton color={'#E9C6A3'} border={true} className='border-[#E9C6A3] bg-[#FFE1C2] border-4'><KawaiiTitle size={'2xl'}>にっきをみる</KawaiiTitle></KawaiiButton>
+                        <KawaiiButton color={'#7A5648'} border={true} className='border-4'>ホーム</KawaiiButton>
+                    </div>
+                </div>
+            </KawaiiPage>
         </>
     )
 }
